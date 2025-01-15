5843eadb5af5697c62d0699c6ecbea357d8bf71b
Ron Almog <ron8132@gmail.com>
2025-01-15 17:46:58 -0500

    allow image url on prod

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,15 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "google.ca",
-        port: "",
-        pathname: "/",
-        search: "",
+        hostname: "ticket-master-nextjs15.vercel.app",
       },
       {
         protocol: "https",
         hostname: "strong-clownfish-676.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "groovy-ferret-147.convex.cloud",
       },
     ],
   },
